36347987d9bee3f2b38f928ba7c69dede0eacefc
zhangleizhl90 <q2237@126.com>
2016-09-20 17:15:59 +0800

    实现自动滚动

--- app/src/main/java/me/zhanglei/bannerview/MainActivity.java
+++ app/src/main/java/me/zhanglei/bannerview/MainActivity.java
@@ -1,13 +1,38 @@
 package me.zhanglei.bannerview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.zhanglei.widgets.BannerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BannerView mBannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBannerView = (BannerView) findViewById(R.id.banner_view);
+        List<Bitmap> bitmapList = new ArrayList<>(2);
+        bitmapList.add(BitmapFactory.decodeResource(getResources(), R.drawable.res_1));
+        bitmapList.add(BitmapFactory.decodeResource(getResources(), R.drawable.res_2));
+        bitmapList.add(BitmapFactory.decodeResource(getResources(), R.drawable.res_3));
+        bitmapList.add(BitmapFactory.decodeResource(getResources(), R.drawable.res_4));
+        mBannerView.update(bitmapList);
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        mBannerView.startAutoScroll();
     }
 }
--- widgets/src/main/java/me/zhanglei/widgets/BannerView.java
+++ widgets/src/main/java/me/zhanglei/widgets/BannerView.java
@@ -1,5 +1,6 @@
 package me.zhanglei.widgets;
 
+import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -7,22 +8,32 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * Banner View
  * Created by zhanglei on 2016/9/17 0017.
  */
 public class BannerView extends View {
 
+    private static final String TAG = BannerView.class.getSimpleName();
+
     private List<Bitmap> mBitmapList = new ArrayList<>();
     private int mCurrentIndex = -1;
 
+    private OnItemClickListener mOnItemClickListener;
+
     private DrawHelper mDrawHelper;
+    private AnimatorHelper mAnimatorHelper;
+    private ScrollHelper mScrollHelper;
 
     public BannerView(Context context) {
         super(context);
@@ -47,6 +58,8 @@ public class BannerView extends View {
 
     private void init() {
         mDrawHelper = new DrawHelper();
+        mAnimatorHelper = new AnimatorHelper();
+        mScrollHelper = new ScrollHelper();
     }
 
     @Override
@@ -55,7 +68,7 @@ public class BannerView extends View {
 
         int maxHeight = 0;
         for (Bitmap bitmap : mBitmapList) { // find max height
-            int height = (int) (bitmap.getHeight() * (getMeasuredWidth() / (double)bitmap.getWidth()));
+            int height = (int) (bitmap.getHeight() * (getMeasuredWidth() / (double) bitmap.getWidth()));
             maxHeight = maxHeight < height ? height : maxHeight;
         }
 
@@ -69,14 +82,208 @@ public class BannerView extends View {
         mDrawHelper.draw(canvas);
     }
 
+//    private boolean mFakingDragging = false;
+//    private VelocityTracker mVelocityTracker;
+//    private float mLastMotionX;
+//    private float mLastMotionY;
+//    private float mInitialMotionX;
+//    private float mInitialMotionY;
+//    private int mActivePointerId;
+//    private boolean mIsBeingDragged;
+//    private float mMaximumVelocity;
+//
+//    @Override
+//    public boolean onTouchEvent(MotionEvent ev){
+//        if (mFakingDragging) {
+//            return true;
+//        }
+//
+//        if (ev.getAction() == MotionEvent.ACTION_DOWN && ev.getEdgeFlags() != 0) {
+//            return false;
+//        }
+//
+//        if (mBitmapList == null || mBitmapList.isEmpty()) {
+//            return false;
+//        }
+//
+//        if (mVelocityTracker == null) {
+//            mVelocityTracker = VelocityTracker.obtain();
+//        }
+//        mVelocityTracker.addMovement(ev);
+//
+//        final int action = ev.getAction();
+//        boolean needsInvalidate = false;
+//
+//        switch (action & MotionEventCompat.ACTION_MASK) {
+//            case MotionEvent.ACTION_DOWN: {
+//                mScroller.abortAnimation();
+//                mPopulatedPending = false;
+//                populate();
+//
+//                mLastMotionX = mInitialMotionX = ev.getX();
+//                mLastMotionY = mInitialMotionY = ev.getY();
+//                mActivePointerId = ev.getPointerId(0);
+//                break;
+//            }
+//            case MotionEvent.ACTION_MOVE:
+//                if (!mIsBeingDragged) {
+//                    final int pointerIndex = ev.findPointerIndex(mActivePointerId);
+//                    if (pointerIndex == -1) {
+//                        needsInvalidate = resetTouch();
+//                        break;
+//                    }
+//
+//                    final float x = ev.getX(pointerIndex);
+//                    final float xDiff = Math.abs(x - mLastMotionX);
+//                    final float y = ev.getY(pointerIndex);
+//                    final float yDiff = Math.abs(y - mLastMotionY);
+//
+//                    if (xDiff > mTouchSlop && xDiff > yDiff) {
+//                        mIsBeingDragged = true;
+//                        requestParentDisallowInterceptTouchEvent(true);
+//                        mLastMotionX = x - mInitialMotionX > 0 ? mInitialMotionX + mTouchSlop :
+//                                mInitialMotionX - mTouchSlop;
+//                        mLastMotionY = y;
+//                        setScrollState(SCROLL_STATE_DRAGGING);
+//                        setScrollinfCacheEnable(true);
+//
+//                        ViewParent parent = getParent();
+//                        if (parent != null) {
+//                            parent.requestDisallowInterceptTouchEvent(true);
+//                        }
+//                    }
+//                }
+//                if (mIsBeingDragged) {
+//                    final int activePointerIndex = ev.findPointerIndex(mActivePointerId);
+//                    final float x = ev.getX(activePointerIndex);
+//                    needsInvalidate |= performDrag(x);
+//                }
+//                break;
+//            case MotionEvent.ACTION_UP:
+//                if (mIsBeingDragged) {
+//                    final VelocityTracker velocityTracker = mVelocityTracker;
+//                    velocityTracker.computeCurrentVelocity(1000, mMaximumVelocity);
+//                    int initialVelocity = (int) VelocityTrackerCompat.getXVelocity(velocityTracker,
+//                            mActivePointerId);
+//                    mPopulatePending = true;
+//                    final int width = getClientWidth();
+//                    final int scrollX = getScrollX();
+//                    final ItemInfo ii = infoForCurrentScrollPosition();
+//                    final float marginOffset = (float) mPageMargin / width;
+//                    final int currentPage = ii.position;
+//                    final float pageOffset = (((float) scrollX / width) - ii.offset)
+//                            / (ii.widthFactor + marginOffset);
+//                    final int activiePointerIndex = ev.findPointerIndex(mActivePointerId);
+//                    final float x = ev.getX(activiePointerIndex);
+//                    final int totalDelta = (int) (x - mInitialMotionX);
+//                    int nextPage = determineTargetPage(currentPage, pageOffset, initialVelocity,
+//                            totalDelta);
+//                    setCurrentItemInternal(nextpage, true, true, initialVelocity);
+//
+//                    needsInvalidate = resetTouch();
+//                }
+//                break;
+//            case MotionEvent.ACTION_CANCEL:
+//                if (mIsBeingDragged) {
+//                    scrollToItem(mCurItem, true, 0, false);
+//                    needInvalidate = resetTouch();
+//                }
+//                break;;
+//            case MotionEventCompat.ACTION_POINTER_DOWN: {
+//                final int index = MotionEventCompat.getActionIndex(e);
+//                final float x = ev.getX(index);
+//                mLastMotionX = x;
+//                mActivePointerId = ev.getPointerId(index);
+//                break;
+//            }
+//            case MotionEventCompat.ACTION_POINTER_UP:
+//                opnSecondaryPointerUp(ev);
+//                mLastMotionX = ev.getX(ev.findPointerIndex(mActivePointerId));
+//                break;
+//        }
+//        if (needsInvalidate) {
+//            ViewCompat.postInvalidateOnAnimation(this);
+//        }
+//
+//        return true;
+//    }
+//
+//    private boolean resetTouch() {
+//        boolean needsInvalidate;
+//        mActivePointerId = INVALID_POINTER;
+//        endDrag();
+//        needsInvalidate = mLeftEdge.onRelease() | mRightEdge.onRelease();
+//        return needsInvalidate;
+//    }
+//
+//    private void requestParentDisallowInterceptTouchEvent(boolean disallowIntercept) {
+//        final ViewParent parent = getParent();
+//        if (parent != null) {
+//            parent.requestDisallowInterceptTouchEvent(disallowIntercept);
+//        }
+//    }
+//
+//    private boolean performDrag(float x) {
+//        boolean needInvalidate = false;
+//
+//        final float deltaX = mLastMotionX - x;
+//        mLastMotionX = x;
+//
+//        float oldScrollX = getScrollX();
+//        float scrollX = oldScrollX + deltaX;
+//        final int width = getClientWidth();
+//
+//        float leftBound = width * mFirstOffset;
+//        float rightBound = width * mLastOffset;
+//        boolean leftAbsolute = true;
+//        boolean rightAbsolute = true;
+//
+//        final ItemInfo
+//    }
+
     public void update(List<Bitmap> bitmapList) {
         mBitmapList.clear();
 
         if (bitmapList != null) {
             mBitmapList.addAll(bitmapList);
+            mCurrentIndex = 0;
         }
 
         requestLayout();
+    }
+
+    private static final int MESSAGE_SCROLL = 0;
+
+    private WeakReference<Handler> mHandlerRef;
+
+    public void startAutoScroll() {
+        int newIndex = (mCurrentIndex + 1) % mBitmapList.size();
+        mScrollHelper.scrollToIndex(newIndex);
+        mCurrentIndex = newIndex;
+
+        setHandler();
+
+        Handler handler = mHandlerRef.get();
+        if (handler != null) {
+            handler.removeMessages(MESSAGE_SCROLL);
+            handler.sendEmptyMessageDelayed(MESSAGE_SCROLL, 2000);
+        }
+    }
+
+    private void setHandler() {
+        if (mHandlerRef == null || mHandlerRef.get() == null) {
+            mHandlerRef = new WeakReference<Handler>(new Handler() {
+                @Override
+                public void handleMessage(Message msg) {
+                    super.handleMessage(msg);
+                    startAutoScroll();
+                }
+            });
+        }
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        mOnItemClickListener = onItemClickListener;
     }
 
     private class DrawHelper {
@@ -88,12 +295,14 @@ public class BannerView extends View {
 
         public DrawHelper() {
             mPaint = new Paint();
+            mOffset = 0;
         }
 
         public void update(int offset, int firstIndex, int secondIndex) {
             mOffset = offset;
             mFirstIndex = firstIndex;
             mSecondIndex = secondIndex;
+            invalidate();
         }
 
         public void draw(Canvas canvas) {
@@ -137,8 +346,8 @@ public class BannerView extends View {
             rect.left = offset;
             rect.top = 0;
             rect.right = rect.left + getWidth();
-            int height = (int) (bitmap.getHeight() * bitmap.getWidth() / (double) getWidth());
-            rect.bottom = rect.top +height;
+            int height = (int) (bitmap.getHeight() * getWidth() / (double) bitmap.getWidth());
+            rect.bottom = rect.top + height;
             return rect;
         }
 
@@ -151,8 +360,54 @@ public class BannerView extends View {
         }
     }
 
+    private class AnimatorHelper {
+        public void startAnimator(final int firstIndex, final int secondIndex) {
+            ValueAnimator animator = ValueAnimator.ofInt(0, -getWidth());
+            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    int offset = (int) animation.getAnimatedValue();
+                    mDrawHelper.update(offset, firstIndex, secondIndex);
+                }
+            });
+            animator.setInterpolator(new LinearInterpolator());
+            animator.setDuration(200).start();
+        }
+    }
+
     private class ScrollHelper {
         static final int LEFT_TO_RIGHT = 0x1000;
         static final int RIGHT_TO_LEFT = 0x1001;
+
+        private int mFromIndex;
+        private int mToIndex;
+
+        public void scrollToIndex(int index) {
+            scrollFromTo(mCurrentIndex, index);
+        }
+
+        public void scrollFromTo(int from, int to) {
+            mFromIndex = from;
+            mToIndex = to;
+            scroll();
+        }
+
+        private void scroll() {
+            if (mFromIndex == mToIndex) {
+                return;
+            }
+
+            if (mFromIndex > mToIndex) {
+                mAnimatorHelper.startAnimator(mFromIndex, mFromIndex - 1);
+                --mFromIndex;
+            } else {
+                mAnimatorHelper.startAnimator(mFromIndex, mFromIndex + 1);
+                ++mFromIndex;
+            }
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int index);
     }
 }
